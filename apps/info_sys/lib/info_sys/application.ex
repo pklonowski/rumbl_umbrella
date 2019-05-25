18140723101f9ed5788dc789614838c479baad34
pklonowski <paul.klonowski@gmail.com>
2019-05-25 01:06:43 -0600

    To mid p.258

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -10,6 +10,7 @@ defmodule InfoSys.Application do
     children = [
       # Starts a worker by calling: InfoSys.Worker.start_link(arg)
       # {InfoSys.Worker, arg}
+      #InfoSys.Supervisor
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
